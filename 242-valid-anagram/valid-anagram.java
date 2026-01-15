3a351d092eb836e25001bffae6ac58657c4223cd
Pratiksha091920 <pratikshakarlekar2005@gmail.com>
2026-01-15 15:05:50 +0530

    Time: 4 ms (79.28%) | Memory: 44.2 MB (85.95%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int m=s.length();
+        int n=t.length();
+
+        if(m!=n){
+            return false;
+        }
+
+        int count[]=new int[26];
+
+        for(int i=0;i<m;i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+
+        for(int c:count){
+            if(c!=0){
+                return false;
+            }
+        }
+        return true;
+
+        
+    }
+}
