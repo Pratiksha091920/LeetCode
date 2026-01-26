a4fcfe813f52dbfeeeea03d66f4123ebcd73475c
Pratiksha091920 <pratikshakarlekar2005@gmail.com>
2026-01-26 10:57:03 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (17.66%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,26 +1,36 @@
 class Solution {
     public int missingNumber(int[] nums) {
 
-        int i=0;
-        int N=nums.length;
-        while(i<N){
-            int correctIndex=nums[i];
-            if(nums[i]<nums.length && nums[i]!=nums[correctIndex]){
-                int temp=nums[i];
-                nums[i]=nums[correctIndex];
-                nums[correctIndex]=temp;
-            }
-            else{
-                i++;
-            }
-        }
+        // int i=0;
+        // int N=nums.length;
+        // while(i<N){
+        //     int correctIndex=nums[i];
+        //     if(nums[i]<nums.length && nums[i]!=nums[correctIndex]){
+        //         int temp=nums[i];
+        //         nums[i]=nums[correctIndex];
+        //         nums[correctIndex]=temp;
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
 
-        for(int index=0;index<nums.length;index++){
-            if(nums[index]!=index){
-                return index;
-            }
-        }
-        return nums.length;
+        // for(int index=0;index<nums.length;index++){
+        //     if(nums[index]!=index){
+        //         return index;
+        //     }
+        // }
+        // return nums.length;
         
+        int xor=0;
+        for(int i=0;i<=nums.length;i++){
+        xor=xor^i;
+        }
+        for(int n:nums){
+        xor=xor^n;
+        }
+        
+
+        return xor;
     }
 }
