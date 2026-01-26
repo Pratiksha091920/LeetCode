e0c7c99d7edca817f237168c344c0b975c8891ad
Pratiksha091920 <pratikshakarlekar2005@gmail.com>
2026-01-26 11:39:34 +0530

    Time: 1 ms (88.78%) | Memory: 42.4 MB (90.59%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,16 +1,32 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        for(int i=0;i<16;i++){
-            int powerofFour=(int) Math.pow(4,i);
-            if(powerofFour==n){
-            return true;
-        }
-        if(powerofFour>n){
-            return false;
-        }
-        }
+        // for(int i=0;i<16;i++){
+        //     int powerofFour=(int) Math.pow(4,i);
+        //     if(powerofFour==n){
+        //     return true;
+        // }
+        // if(powerofFour>n){
+        //     return false;
+        // }
+        // }
         
 
-        return false;
+        // return false;
+      
+        // If 'n' is 1, it is a power of four
+        if (n == 1)
+            return true;
+        
+        // If 'n' is non-positive, it cannot be a power of four
+        if (n <= 0)
+            return false; 
+        
+        // Calculate the logarithm of 'n' with base 4
+        double ans = Math.log(n) / Math.log(4);
+        
+        // Check if the result of the logarithmic operation is an integer
+        return (ans==(int)ans);
+    
+
     }
 }
