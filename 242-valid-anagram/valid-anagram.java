ebe41004f486fcb2e2db3fc6f40ced20eec07c23
Pratiksha091920 <pratikshakarlekar2005@gmail.com>
2026-07-30 00:35:26 +0530

    Time: 16 ms (20.83%) | Memory: 46.4 MB (34.62%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,24 +3,44 @@ class Solution {
         int m=s.length();
         int n=t.length();
 
-        if(m!=n){
-            return false;
+        // if(m!=n){
+        //     return false;
+        // }
+
+        // int count[]=new int[26];
+
+        // for(int i=0;i<m;i++){
+        //     count[s.charAt(i)-'a']++;
+        //     count[t.charAt(i)-'a']--;
+        // }
+
+        // for(int c:count){
+        //     if(c!=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char ch:s.toCharArray()){
+           map.put(ch,map.getOrDefault(ch,0)+1);
         }
 
-        int count[]=new int[26];
 
-        for(int i=0;i<m;i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
-        }
-
-        for(int c:count){
-            if(c!=0){
-                return false;
+        for(char ch:t.toCharArray()){
+            if(!map.containsKey(ch)){
+               return false;
             }
-        }
-        return true;
 
+            map.put(ch,map.get(ch)-1);
+
+            if(map.get(ch)==0){
+                map.remove(ch);
+            }
+
+        }
+    
+    return map.isEmpty();
         
     }
 }
