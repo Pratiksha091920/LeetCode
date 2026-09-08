f154c13cdb763f37e1e0f44a3849f0920dc36ce6
Pratiksha091920 <pratikshakarlekar2005@gmail.com>
2026-09-08 23:39:42 +0530

    Time: 11 ms (96.31%) | Memory: 47.8 MB (33.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,31 +1,66 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        HashMap<Integer,Integer> map=new HashMap<>();
+        // HashMap<Integer,Integer> map=new HashMap<>();
 
-        int[] result=new int[k];
+        // int[] result=new int[k];
+
+        // for(int i=0;i<nums.length;i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+
+        // for(int i=0;i<k;i++){
+        //     int maxnum=0;
+        //     int maxfreq=0;
+
+        //     for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+        //         if(entry.getValue()> maxfreq){
+        //             maxfreq=entry.getValue();
+        //             maxnum=entry.getKey();
+        //         }
+
+        //     }
+
+        //     result[i]=maxnum;
+        //     map.remove(maxnum);
+        // }
+
+        // return result;
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+        
 
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
 
-        for(int i=0;i<k;i++){
-            int maxnum=0;
-            int maxfreq=0;
+        ArrayList<Integer>[] bucket=new ArrayList[nums.length+1];
+       
+         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            int num=entry.getKey();
+            int freq=entry.getValue();
 
-            for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-                if(entry.getValue()> maxfreq){
-                    maxfreq=entry.getValue();
-                    maxnum=entry.getKey();
-                }
-
+            if(bucket[freq]==null){
+                bucket[freq]=new ArrayList<>();
             }
 
-            result[i]=maxnum;
-            map.remove(maxnum);
-        }
+            bucket[freq].add(num);
+         }
+        int index=0;
+        int result[]=new int[k];
+         for(int freq=bucket.length-1;freq>=0 && index< k;freq--){
+            if(bucket[freq]!=null){
+                for(int num:bucket[freq]){
+                    result[index]=num;
+                    index++;
 
-        return result;
+                    if(index==k){
+                        break;
+                    }
+                }
+            }
+         }
+       return result;
         
     }
 }
